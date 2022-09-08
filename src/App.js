b1a4aed7d0db7f8ff5fde78057b0e0723e1dd540
Aliyu abdulrazak <aliyuabdulrazaks539@gmail.com>
2022-09-08 09:37:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChatEngine
+      height='100vh'
+      projectID="
+0d935d60-d993-49c0-8e2b-be414077d217"
+      userName="Abdulrazak"
+      userSecret="0067"
+     renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+    />
+  )
 }
+
+
 
 export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+import axios from 'axios'
+
+
+import { useState } from "react"
+
+const LoginForm = () =>{
+    const [userName, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+}
